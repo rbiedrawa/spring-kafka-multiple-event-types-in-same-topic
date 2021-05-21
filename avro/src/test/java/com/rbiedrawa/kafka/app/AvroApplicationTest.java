6c194ae68b92945030a7972448842cc10843e7cd
rbiedrawa <biedrawa.rafal@gmail.com>
2021-05-21 15:27:34 +0200

    Init commit

---
+++ avro/src/test/java/com/rbiedrawa/kafka/app/AvroApplicationTest.java
@@ -0,0 +1,13 @@
+package com.rbiedrawa.kafka.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvroApplicationTest {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ proto/src/main/java/com/rbiedrawa/kafka/app/ProtoApplication.java
@@ -0,0 +1,13 @@
+package com.rbiedrawa.kafka.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProtoApplication.class, args);
+	}
+
+}
